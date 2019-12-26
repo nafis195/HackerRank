8883d5d1e02afd7d70a03f3acd48c2981c82d304
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-25 20:08:43 -0800

    Add files via upload

--- Queue using Two Stacks/Source.cpp
+++ Queue using Two Stacks/Source.cpp
@@ -1,1 +1,37 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Hackerrank - Queue using Two Stacks
+// Problem Statement - https://www.hackerrank.com/challenges/queue-using-two-stacks/problem
+// Main.cpp
 
+
+#include<iostream>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int main()
+{
+	queue <long long int> q;
+	int type, query;
+	long long int x;
+
+	cin >> query;
+	for (int i = 0; i < query; i++)
+	{
+		cin >> type;
+		if (type == 1)
+		{
+			cin >> x;
+			q.push(x);
+		}
+		else if (type == 2)
+			q.pop();
+		else if (type == 3)
+		{
+			cout << q.front() << endl;
+		}
+	}
+
+	return 0;
+}
